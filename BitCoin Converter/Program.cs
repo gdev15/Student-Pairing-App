4f995f623ff8493db1f6a57351e328f103f56343
John Black <john.m.black.16@gmail.com>
2023-12-04 15:18:00 -0600

    BitCoin Converter Project start

---
+++ BitCoin Converter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BitCoin_Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
