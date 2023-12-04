0223ad87c0d16d973c1acb4b3a2d426b15daeae1
John Black <john.m.black.16@gmail.com>
2023-12-04 14:04:51 -0600

    Created a Student Class

---
+++ Student_Pairing_Application/Student_Pairing_Application/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Student_Pairing_Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Students student1 = new()
+            {
+                Name = "Mike "
+            };
+        }
+    }
+}
---
+++ Student_Pairing_Application/Student_Pairing_Application/Students.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Pairing_Application
+{
+    public class Students
+    {
+        //fields
+
+        //props
+        public string Name { get; set; }
+
+        //ctors
+
+        //methods
+        public Students() { }
+        public Students(string name)
+        {
+            Name = name;
+        }
+    }
+}
